5db2574c8864964ef73d3a2a30038f7db38054d2
mapdonnelly <madonnelly@ucdavis.edu>
2018-11-14 16:22:59 -0800

    temp data loops
    Added a file to loop through models provided on the berkeley ftp.
    Downloaded data and stored them in a file on my computer.

--- LOCA_FTP_loop.R
+++ LOCA_FTP_loop.R
@@ -1,104 +1,64 @@
 rm(list = ls())
 library(ncdf4)
 library(tidyverse)
+library(parallel)
 setwd('~/Desktop/GitHub/CDPH_heat_project/')
 
-tempMat <- c("tasmax")
-names(tempMat) <- c("Daily max temp")
 
-rcpMat <- c("45","85")
-names(rcpMat) <- c("RCP4.5 (emissions peak 2040, stabiliazation by 2100)","RCP8.5 (emissions continue to rise throughout the 21st century)")
+#rcpMat <- c("45","85")
+#names(rcpMat) <- c("RCP4.5 (emissions peak 2040, stabiliazation by 2100)","RCP8.5 (emissions continue to rise throughout the 21st century)")
 
-modelMat <- c("ACCESS1-0","CanESM2","CCSM4","CESM1-BGC","CMCC-CMS","CNRM-CM5","GFDL-CM3","HadGEM2-CC","HadGEM2-ES","MIROC5")
-names(modelMat) <- c("ACCESS1-0","CanESM2","CCSM4","CESM1-BGC","CMCC-CMS","CNRM-CM5","GFDL-CM3","HadGEM2-CC","HadGEM2-ES","MIROC5")
+modelMat <- c("ACCESS1-0","CanESM2","CESM1-BGC","CMCC-CMS","CNRM-CM5","GFDL-CM3","HadGEM2-CC","HadGEM2-ES","MIROC5")
+names(modelMat) <- c("ACCESS1-0","CanESM2","CESM1-BGC","CMCC-CMS","CNRM-CM5","GFDL-CM3","HadGEM2-CC","HadGEM2-ES","MIROC5")
 
-yearStartMat <- c("20300101")
-names(yearStartMat) <- c("20300101")
+download_nc <- function(modelVar,YearStartVar,YearStopVar){
+  # n<-5
+  # modelVar<-n
+  model <- modelVar
+  #rcp <- rcpVar
+  #temp <- tempVar 
+  yearStart<-YearStartVar
+  yearStop<-YearStopVar
+  
+  # ncdfURL.45.tasmax <- paste0("http://albers.cnr.berkeley.edu/data/scripps/loca/",model,"/rcp45/tasmax/tasmax_day_",model,"_rcp45_r6i1p1_",yearStart,"-",yearStop,".LOCA_2016-04-02.16th.CA_NV.nc")
+  # dest.45.tasmax <-  paste0("~/Desktop/GitHub/CDPH_heat_project/data/",model,"_rcp45_tasmax_",yearStart,"_",yearStop,"_","CA_NV.nc")
+  # download.file(url=ncdfURL.45.tasmax,destfile=dest.45.tasmax, mode = "wb")
+  # 
+  #  ncdfURL.45.tasmin <- paste0("http://albers.cnr.berkeley.edu/data/scripps/loca/",model,"/rcp45/tasmin/tasmin_day_",model,"_rcp45_r6i1p1_",yearStart,"-",yearStop,".LOCA_2016-04-02.16th.CA_NV.nc")
+  #  dest.45.tasmin <-  paste0("~/Desktop/GitHub/CDPH_heat_project/data/",model,"_rcp45_tasmin_",yearStart,"_",yearStop,"_","CA_NV.nc") 
+  #  download.file(url=ncdfURL.45.tasmin,destfile=dest.45.tasmin, mode = "wb") 
+  # 
+  # ncdfURL.85.tasmax <- paste0("http://albers.cnr.berkeley.edu/data/scripps/loca/",model,"/rcp85/tasmax/tasmax_day_",model,"_rcp85_r6i1p1_",yearStart,"-",yearStop,".LOCA_2016-04-02.16th.CA_NV.nc")
+  # dest.85.tasmax <-  paste0("~/Desktop/GitHub/CDPH_heat_project/data/",model,"_rcp85_tasmax_",yearStart,"_",yearStop,"_","CA_NV.nc")
+  # download.file(url=ncdfURL.85.tasmax,destfile=dest.85.tasmax, mode = "wb")
+  # 
+  #  ncdfURL.85.tasmin <- paste0("http://albers.cnr.berkeley.edu/data/scripps/loca/",model,"/rcp85/tasmin/tasmin_day_",model,"_rcp85_r6i1p1_",yearStart,"-",yearStop,".LOCA_2016-04-02.16th.CA_NV.nc")
+  #  dest.85.tasmin <-  paste0("~/Desktop/GitHub/CDPH_heat_project/data/",model,"_rcp85_tasmin_",yearStart,"_",yearStop,"_","CA_NV.nc") 
+  #  download.file(url=ncdfURL.85.tasmin,destfile=dest.85.tasmin, mode = "wb") 
+  # 
+  ##for CCSM4 you need to change r1i1p1 to r6i1p1
 
-yearStopMat <- c("20301231")
-names(yearStopMat) <- c("20301231")
+  ncdfURL.hist.tasmax <- paste0("http://albers.cnr.berkeley.edu/data/scripps/loca/",model,"/historical/tasmax/tasmax_day_",model,"_historical_r1i1p1_",yearStart,"-",yearStop,".LOCA_2016-04-02.16th.CA_NV.nc")
+  dest.hist.tasmax <-  paste0("~/Desktop/GitHub/CDPH_heat_project/data/",model,"_historical_tasmax_",yearStart,"_",yearStop,"_","CA_NV.nc")
+  download.file(url=ncdfURL.hist.tasmax,destfile=dest.hist.tasmax, mode = "wb")
 
-process_single_nc <- function(modelVar, rcpVar, popVar){
-  n<-3
-  modelVar<-n
-  rcpVar<-1
-  tempVar<-1
-  yearStartVar<-1
-  yearStopVar<-1
-  model <- modelMat[modelVar]
-  rcp <- rcpMat[rcpVar]
-  temp <- tempMat[tempVar] 
-  yearStart<-yearStartMat[yearStartVar]
-  yearStop<-yearStopMat[yearStopVar]
-  ###What is dname???
-  dname = "hectares"
-  
-  ##for CCSM$ you need to change r1i1p1 to r6i1p1
-  ncdfURL <- paste0("http://albers.cnr.berkeley.edu/data/scripps/loca/",model,"/rcp",rcp,"/",temp,"/",temp,"_day_",model,"_rcp",rcp,"_r1i1p1_",yearStart,"-",yearStop,".LOCA_2016-04-02.16th.CA_NV.nc")
-  dest <-  paste0("~/Desktop/GitHub/CDPH_heat_project/data/",model,"_rcp",rcp,"_",temp,"_",yearStart,"_",yearStop,"_","CA_NV.nc") 
-  download.file(url=ncdfURL,destfile=dest, mode = "wb") 
-  
-  ncin <- nc_open(dest)
-  # store values from variables and atributes
-  attributes(ncin$dim)$names
-  nc_lat <- ncvar_get(ncin, "lat")
-  n_lat <- dim(nc_lat)
-  nc_lon <- ncvar_get(ncin, "lon")
-  n_lon <- dim(nc_lon)
-  nc_year <- ncvar_get(ncin, "time")+1953
-  n_year <- dim(nc_year)
-  
-  # get hectares
-  tmp_array <- ncvar_get(ncin,dname)
-  dlname <- ncatt_get(ncin,dname,"long_name")
-  dunits <- ncatt_get(ncin,dname,"units")
-  fillvalue <- ncatt_get(ncin,dname,"_FillValue")
-  dim(tmp_array)
-  
-  # get global attributes
-  # title <- ncatt_get(ncin,0,"title")
-  # institution <- ncatt_get(ncin,0,"institution")
-  # datasource <- ncatt_get(ncin,0,"source")
-  # references <- ncatt_get(ncin,0,"references")
-  # history <- ncatt_get(ncin,0,"history")
-  # Conventions <- ncatt_get(ncin,0,"Conventions")
-  
-  nc_close(ncin)
-  
-  # replace netCDF fill values with NA's
-  tmp_array[tmp_array==fillvalue$value] <- NA
-  
-  # check number of missing data values
-  # length(na.omit(as.vector(tmp_array[,,1])))
-  # dimnames(tmp_array) <-list(lon=nc_lon, lat=nc_lat, year=nc_year)
-  
-  bar <- data.frame(expand.grid(lon = nc_lon, lat = nc_lat)) %>%
-    cbind(matrix(as.vector(tmp_array), nrow = n_lon*n_lat, ncol = n_year)) %>% 
-    mutate(model=model, scenario = rcp, population = pop) %>% 
-    select(model, scenario, population, lon, lat, 3:150)
-  
-  names(bar) <- c("model","scenario","population","lon","lat",1954:2100) 
-  
-  
-  CAonly <- inner_join(bar, locationData) %>%
-    gather(6:152, key = "year", value = "hectares") %>% 
-    mutate(year = as.integer(as.character(year))) %>% 
-    mutate(period = factor(ifelse(year %in% c(1961:1990),"baseline (1961-1990)", 
-                                  ifelse(year %in% c(2000:2020), "early (2000-2020)",
-                                         ifelse(year %in% c(2040:2060),"mid-century (2040-2060)",
-                                                ifelse(year %in% c(2080:2100), "late-century (2080-2100)", "between")))),
-                           levels = c("baseline (1961-1990)","early (2000-2020)","mid-century (2040-2060)","late-century (2080-2100)","between"))) %>%
-    mutate(climateModel = factor(ifelse(model == "CanESM2","CanESM2 (average)",
-                                        ifelse(model == "CNRM-CM5","CNRM-CM5 (Cool/Wet)",
-                                               ifelse(model == "HadGEM2-ES","HadGEM2-ES (Warm/Dry)","MIROC5 (Complement/Covers a range of outputs"))),
-                                 levels = c("CanESM2 (average)", "CNRM-CM5 (Cool/Wet)","HadGEM2-ES (Warm/Dry)","MIROC5 (Complement/Covers a range of outputs)")),
-           
-           RCP = factor(ifelse(scenario == "45", "RCP4.5 (emissions peak 2040, stabiliazation by 2100)","RCP8.5 (emissions continue to rise throughout the 21st century)"),
-                        levels = c("RCP4.5 (emissions peak 2040, stabiliazation by 2100)","emissions continue to rise throughout the 21st century")), 
-           
-           PopulationGrowth = factor(ifelse(population == "AA.all.bau.mu.nc","Central Projection",
-                                            ifelse(population == "AA.all.L.mu.nc","Low Projection","High Projection")),
-                                     levels = c("Low Projection", "Central Projection","High Projection")))
-  return(CAonly)
+  ncdfURL.hist.tasmin <- paste0("http://albers.cnr.berkeley.edu/data/scripps/loca/",model,"/historical/tasmin/tasmin_day_",model,"_historical_r1i1p1_",yearStart,"-",yearStop,".LOCA_2016-04-02.16th.CA_NV.nc")
+  dest.hist.tasmin <-  paste0("~/Desktop/GitHub/CDPH_heat_project/data/",model,"_historical_tasmin_",yearStart,"_",yearStop,"_","CA_NV.nc")
+  download.file(url=ncdfURL.hist.tasmin,destfile=dest.hist.tasmin, mode = "wb")
 }
+
+
+#rcpVar<- c("historical")##inputs are "rcp45" "rcp85" and "historical"
+#tempVar<-c("tasmax")
+YearStartVar<-c("20050101")
+YearStopVar<-c("20051231")
+#modelMat <- c("CCSM4")
+#names(modelMat) <- c("CCSM4")
+
+
+##Download data year by year splitting it up on multiple cores. 
+mclapply(modelMat, download_nc,YearStartVar=YearStartVar,YearStopVar=YearStopVar) 
+##need to do CCSM$ separately because it has r6i1p1 instead of r1i1p1
+
+
 
---
+++ netcdf_exploration.R
@@ -0,0 +1,83 @@
+##Opening NetCDF files
+##tutorial from this website: http://geog.uoregon.edu/bartlein/courses/geog607/Rmd/netCDF_01.htm
+library(chron)
+library(RColorBrewer)
+library(lattice)
+library(ncdf4)
+setwd('~/Desktop/GitHub/CDPH_heat_project/data/')
+
+ncpath<-"~/Desktop/GitHub/CDPH_heat_project/data/RCP45/"
+ncname<-"ACCESS1-0_rcp45_tasmax_20060101_20061231_CA_NV"
+ncfname<-paste(ncpath,ncname, ".nc",sep = "")
+dname<-"tasmax"
+ncin<-nc_open(ncfname)
+print(ncin)
+##time units are days since 1900-01-01
+lon<-ncvar_get(ncin, "lon")-360
+nlon<-dim(lon)
+head(lon) ##we should subtract 360 from longitude to get in the correct units. 
+lat<-ncvar_get(ncin, "lat")
+nlat<-dim(lat)
+head(lat)
+print(c(nlon,nlat))
+time<-ncvar_get(ncin,"time")
+time
+tunits<-ncatt_get(ncin,"time","units")
+nt<-dim(time)
+nt
+tunits
+  
+##get temperature variables
+tmp_array<-ncvar_get(ncin,dname)
+dlname<-ncatt_get(ncin, dname, "long_name")
+dunits<-ncatt_get(ncin, dname,"units")
+fillvalue<-ncatt_get(ncin, dname, "_FillValue")
+dim(tmp_array)
+
+
+##get the global attributes
+title<-ncatt_get(ncin, 0, "title")
+institution<-ncatt_get(ncin, 0, "institution")
+datasource<-ncatt_get(ncin, 0, "source")
+references<-ncatt_get(ncin, 0, "references")
+history<-ncatt_get(ncin,0,"history")
+conventions<-ncatt_get(ncin, 0, "Conventions")
+
+##close the netcdf file
+nc_close()
+ls()
+
+##convert time by first pulling apart components of time since 1900-01-01
+tustr<-strsplit(tunits$value," ")
+tdstr<-strsplit(unlist(tustr)[3],"-")
+tmonth<-as.integer(unlist(tdstr)[2])
+tday<-as.integer(unlist(tdstr)[3])
+tyear<-as.integer(unlist(tdstr)[1])
+chron(time,origin=c(tmonth,tday,tyear))
+
+##replace netCDF fill values with NAs
+tmp_array[tmp_array==fillvalue$value]<-NA
+length(na.omit(head(as.vector(tmp_array[,,1]))))
+
+##pull out a single slice of the array
+m<-1
+tmp_slice<-tmp_array[,,m]-273
+dim(tmp_slice)
+image(lon,lat,tmp_slice, col=rev(brewer.pal(10,"RdBu")))
+
+grid<-expand.grid(lon=lon,lat=lat)
+cutpts<-c(-50,-40,-30,-20,-10,0,10,20,30,40,50)
+levelplot(tmp_slice~lon*lat, data=grid, at=cutpts, cuts=11, pretty=T, col.regions = (rev(brewer.pal(10,"RdBu"))))
+
+##reshape the entire array
+tmp_vec_long<-as.vector(tmp_array)
+length(tmp_vec_long)
+##reshape this vector into a matrix
+tmp_mat<-matrix(tmp_vec_long, nrow=nlon*nlat, ncol=nt)
+dim(tmp_mat)
+head(na.omit(tmp_mat)) ##still in Kelvin
+##create a dataframe
+lonlat<-as.matrix(expand.grid(lon,lat))
+tmp_df02<-data.frame(cbind(lonlat,tmp_mat))
+names(tmp_df02)<-c("lon","lat",1:365)
+head(na.omit(tmp_df02, 10))
